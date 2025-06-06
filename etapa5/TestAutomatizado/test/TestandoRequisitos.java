dee58f7543050f2526ebf24ad5faa4aee433a947
Bruno Kenji <obrunokenji@gmail.com>
2025-06-06 11:15:35 -0300

    meus testes

---
+++ etapa5/TestAutomatizado/test/TestandoRequisitos.java
@@ -0,0 +1,35 @@
+
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author aluno
+ */
+public class TestandoRequisitos {
+    Testando test = new Testando();
+    @Test //teste requisito funcional 1 para cadastrar clietne pessoal
+    public void testRF1clientepessoal(){
+        String nome = "bruno";
+        String cpf = "12345678910";
+        String senha= "1212121212";
+        assertTrue(test.cadastroCP(nome, cpf, senha), "Cadastro invalido");
+    }
+    @Test //test requito funcional 1 para cadastrar cliente corporativo
+    public void testRF1clientecorporativo(){
+        String nome = "empresa";
+        String cnpj = "12345670018910";
+        String senha= "1212121212";
+        double credito = 10000.0;
+        assertTrue(test.cadastroCC(nome, cnpj, senha, credito), "Cadastro invalido");
+    }
+    @Test //test requisito funcional 2 registro de pedido
+    public void testRF2(){
+        String cliente = "bruno";
+        String idProduto = "0119";
+        int qtd = 3;
+        assertEquals("registrado", test.registroPedido(cliente, idProduto, qtd));
+    } 
+
+    
+}
